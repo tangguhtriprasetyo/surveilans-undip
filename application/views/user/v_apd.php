8bb687b08aa92ef126147e00e8061b49bbd4fce7
tangguhprasetyo <tangguhtriprasetyo@gmail.com>
2020-05-20 00:39:14 +0700

    apd

--- application/views/user/v_apd.php
+++ application/views/user/v_apd.php
@@ -40,109 +40,109 @@
                     <input type="radio" id="jenis" name="jenis" value="Faskes Tingkat 1" onclick="document.getElementById('textjenis').disabled = true;">
                     <label for="Faskes Tingkat 1">Faskes tingkat 1</label><br>
                     <input type="radio" id="jenis" name="jenis" value="Faskes Tingkat 2" onclick="document.getElementById('textjenis').disabled = true;">
-                    <label for="Faskes Tingkat 1">Faskes tingkat 2</label><br>
+                    <label for="Faskes Tingkat 2">Faskes tingkat 2</label><br>
                     <input type="radio" id="jenis" name="jenis" value="Faskes Tingkat 3" onclick="document.getElementById('textjenis').disabled = true;">
-                    <label for="Faskes Tingkat 1">Faskes tingkat 3</label><br>
+                    <label for="Faskes Tingkat 3">Faskes tingkat 3</label><br>
                     <input type="radio" id="jenis" onclick="document.getElementById('textjenis').disabled = false;">
-                    <label for="Faskes Tingkat 1"><input type="text" id="textjenis" name="jenis" placeholder="  Lainnya" disabled="disabled"></label><br>
+                    <label for="Lainnya"> <input type="text" id="textjenis" name="jenis" placeholder="  Lainnya" disabled="disabled"> </label><br>
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNama1">2. Nama Instansi</label>
-                    <input type="text" name="nohp" class="form-control" id="exampleInputNoHP" placeholder="Nama Instansi">
+                    <input type="text" name="nama" class="form-control" id="exampleInputNoHP" placeholder="Nama Instansi">
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNama1">3. Alamat Instansi</label>
-                    <input type="text" name="nohp" class="form-control" id="exampleInputNoHP" placeholder="Alamat Instansi">
+                    <input type="text" name="alamat" class="form-control" id="exampleInputNoHP" placeholder="Alamat Instansi">
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNama1">4. Contact Person (Email / Nomer Telfon)</label>
-                    <input type="text" name="nohp" class="form-control" id="exampleInputNoHP" placeholder="">
+                    <input type="text" name="kontak" class="form-control" id="exampleInputNoHP" placeholder="">
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNama1">5. Kabupaten / Kota</label>
-                    <input type="text" name="nohp" class="form-control" id="exampleInputNoHP" placeholder="">
+                    <input type="text" name="daerah" class="form-control" id="exampleInputNoHP" placeholder="">
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNWilayah">6. Apakah Pemerintah Daerah Anda menerapkan skema PSBB ?</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="psbb" value="Ya">
                     <label for="psbb">Ya</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="psbb" value="Tidak">
                     <label for="psbb">Tidak</label>
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNWilayah">7. Apakah pemerintah daerah Anda sudah berkoordinasi dengan instansi Anda terkait kesiapsiagaan dalam penanganan Covid-19 (Surat himbauan, pertemuan, dll) ?</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="koordinasi" value="Sudah">
                     <label for="psbb">Sudah</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="koordinasi" value="Belum">
                     <label for="psbb">Belum</label>
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNWilayah">8. Apakah sudah ada pelatihan terkait penanganan Covid-19 dari Pemerintah/Dinas Kesehatan/ Dinas terkait kepada tenaga kesehatan di instansi Anda ?</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
-                    <label for="psbb">Ya</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
-                    <label for="psbb">Tidak</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="pelatihan" value="Sudah">
+                    <label for="psbb">Sudah</label><br>
+                    <input type="radio" id="psbb" name="pelatihan" value="Belum">
+                    <label for="psbb">Belum</label><br>
+                    <input type="radio" id="psbb" name="pelatihan" value="Tidak Tahu">
                     <label for="psbb">Tidak Tahu</label>
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNWilayah">9. Apakah di Kota/Kabupaten atau wilayah kerja anda sudah terdapat kasus positif Covid-19 ?</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="kasus" value="Sudah">
                     <label for="psbb">Sudah</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="kasus" value="Belum">
                     <label for="psbb">Belum</label>
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNWilayah">10. Apakah di instansi anda menyediakan fasilitas layanan Rapid Test ?</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="rapid" value="Ya">
                     <label for="psbb">Ya</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="rapid" value="Tidak">
                     <label for="psbb">Tidak</label>
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNWilayah">11. Apakah di instansi anda terdapat fasilitas layanan SWAB ?</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="swab" value="Ya">
                     <label for="psbb">Ya</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="swab" value="Tidak">
                     <label for="psbb">Tidak</label>
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNWilayah">12. Apakah tersedia ruang isolasi pasien Covid-19 di instansi anda ?</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="isolasi" value="Ya">
                     <label for="psbb">Ya</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="isolasi" value="Tidak">
                     <label for="psbb">Tidak</label>
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNama1">13. Bagaimana kondisi ruang isolasi pasien Covid-19 ?</label>
-                    <input type="text" name="nohp" class="form-control" id="exampleInputNoHP" placeholder="Jelaskan">
+                    <input type="text" name="kondisi" class="form-control" id="exampleInputNoHP" placeholder="Jelaskan">
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNWilayah">14. Bagaimana ketersediaan hand sanitizer di instansi Anda ?</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="sanitizer" value="Kurang">
                     <label for="psbb">Kurang</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="sanitizer" value="Cukup">
                     <label for="psbb">Cukup</label>
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNWilayah">15. Apakah tersedia thermometer infrared ?</label><br>
-                    <input type="radio" id="psbb" name="kondisi"  value="psbb">
+                    <input type="radio" id="psbb" name="thermo"  value="Ya">
                     <label for="psbb">Ya</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
+                    <input type="radio" id="psbb" name="thermo" value="Tidak">
                     <label for="psbb">Tidak</label>
                   </div>
 
@@ -150,19 +150,19 @@
                     <label for="Tenaga Kesehatan">16. Jumlah tenaga kesehatan yang terlibat dalam penanganan Covid-19</label><br>
 
                       <input type="checkbox" id="checkbox" /> Dokter
-                      <input style="margin: 8px;" type="number" id="textbox" placeholder="  Jumlah" disabled /> </br>
+                      <input style="margin: 8px;" name="dokter" type="number" id="textbox" placeholder="  Jumlah" disabled /> </br>
                       
                       <input type="checkbox" id="checkbox2" /> Petugas Laboran
-                      <input style="margin: 8px;" type="number" id="textbox2" placeholder="  Jumlah" disabled /> </br>
+                      <input style="margin: 8px;" name="laboran" type="number" id="textbox2" placeholder="  Jumlah" disabled /> </br>
                       
                       <input type="checkbox" id="checkbox3" /> Perawat
-                      <input style="margin: 8px;" type="number" id="textbox3" placeholder="  Jumlah" disabled /> </br>
+                      <input style="margin: 8px;" name="perawat" type="number" id="textbox3" placeholder="  Jumlah" disabled /> </br>
                       
                       <input type="checkbox" id="checkbox4" /> Driver Ambulance
-                      <input style="margin: 8px;" type="number" id="textbox4" placeholder="  Jumlah" disabled /> </br>
+                      <input style="margin: 8px;" name="driver" type="number" id="textbox4" placeholder="  Jumlah" disabled /> </br>
                       
                       <input type="checkbox" id="checkbox5" /> Cleaning Service
-                      <input style="margin: 8px;" type="number" id="textbox5" placeholder="  Jumlah" disabled /> </br>
+                      <input style="margin: 8px;" name="cs" type="number" id="textbox5" placeholder="  Jumlah" disabled /> </br>
 
                   </div>
 
@@ -170,57 +170,57 @@
                     <label for="exampleInputNWilayah">17. Bagaimana ketersediaan APD di Instansi Anda dalam satu pekan ini, sebutkan</label><br>
                       
                       <input type="checkbox" id="checkbox6" /> Masker Bedah
-                      <input style="margin: 8px;" type="number" id="textbox6" placeholder="  Jumlah" disabled /> </br>
+                      <input style="margin: 8px;" name="bedah" type="number" id="textbox6" placeholder="  Jumlah" disabled /> </br>
                       
                       <input type="checkbox" id="checkbox7" /> Masker N95
-                      <input style="margin: 8px;" type="number" id="textbox7" placeholder="  Jumlah" disabled /> </br>
+                      <input style="margin: 8px;" name="n95" type="number" id="textbox7" placeholder="  Jumlah" disabled /> </br>
                       
                       <input type="checkbox" id="checkbox8" /> Face Shield
-                      <input style="margin: 8px;" type="number" id="textbox8" placeholder="  Jumlah" disabled /> </br>
+                      <input style="margin: 8px;" name="faceshield" type="number" id="textbox8" placeholder="  Jumlah" disabled /> </br>
                       
                       <input type="checkbox" id="checkbox9" /> Goggle
-                      <input style="margin: 8px;" type="number" id="textbox9" placeholder="  Jumlah" disabled /> </br>
+                      <input style="margin: 8px;" name="goggle" type="number" id="textbox9" placeholder="  Jumlah" disabled /> </br>
                       
                       <input type="checkbox" id="checkbox10" /> Sarung Tangan Medis
-                      <input style="margin: 8px;" type="number" id="textbox10" placeholder="  Jumlah" disabled /> </br>
+                      <input style="margin: 8px;" name="sarungtangan" type="number" id="textbox10" placeholder="  Jumlah" disabled /> </br>
                       
                       <input type="checkbox" id="checkbox11" /> Hazmat
-                      <input style="margin: 8px;" type="number" id="textbox11" placeholder="  Jumlah" disabled /> </br>
+                      <input style="margin: 8px;" name="hazmat" type="number" id="textbox11" placeholder="  Jumlah" disabled /> </br>
                       
                       <input type="checkbox" id="checkbox12" /> Caps
-                      <input style="margin: 8px;" type="number" id="textbox12" placeholder="  Jumlah" disabled /> </br>
+                      <input style="margin: 8px;" name="boot" type="number" id="textbox12" placeholder="  Jumlah" disabled /> </br>
                       
                       <input type="checkbox" id="checkbox13" /> Driver Ambulance
-                      <input style="margin: 8px;" type="number" id="textbox13" placeholder="  Jumlah" disabled /> </br>
+                      <input style="margin: 8px;" name="caps" type="number" id="textbox13" placeholder="  Jumlah" disabled /> </br>
                       
                       <input type="checkbox" id="checkbox14" /> Shoe Cover
-                      <input style="margin: 8px;" type="number" id="textbox14" placeholder="  Jumlah" disabled /> </br>
+                      <input style="margin: 8px;" name="cover" type="number" id="textbox14" placeholder="  Jumlah" disabled /> </br>
 
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNWilayah">18. Sejauh ini APD digunakan berasal dari</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb" onclick="document.getElementById('textbantuan').disabled = true;">
+                    <input type="radio" id="psbb" name="asal" value="Instansi / Mandiri" onclick="document.getElementById('textbantuan').disabled = true;">
                     <label for="psbb">Instansi / Mandiri</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb" onclick="document.getElementById('textbantuan').disabled = true;">
+                    <input type="radio" id="psbb" name="asal" value="Hibah Pemerintah" onclick="document.getElementById('textbantuan').disabled = true;">
                     <label for="psbb">Hibah Pemerintah</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb" onclick="document.getElementById('textbantuan').disabled = true;">
+                    <input type="radio" id="psbb" name="asal" value="Hibah Swasta" onclick="document.getElementById('textbantuan').disabled = true;">
                     <label for="psbb">Hibah Swasta</label><br>
-                    <input type="radio" id="jenis" name="kondisi" value="jenis" onclick="document.getElementById('textbantuan').disabled = false;">
-                    <label for="jenis"><input type="text" id="textbantuan" name="kerja" placeholder="  Lainnya" disabled="disabled"></label><br>
+                    <input type="radio" id="jenis" name="asal" onclick="document.getElementById('textbantuan').disabled = false;">
+                    <label for="jenis"><input type="text" id="textbantuan" name="asal" placeholder="  Lainnya" disabled="disabled"></label><br>
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNWilayah">19. Apakah instansi Anda pernah menerima bantuan APD dari D-DART</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
-                    <label for="psbb">Ya</label><br>
-                    <input type="radio" id="psbb" name="kondisi" value="psbb">
-                    <label for="psbb">Tidak</label>
+                    <input type="radio" id="psbb" name="kondisi" value="Pernah">
+                    <label for="psbb">Pernah</label><br>
+                    <input type="radio" id="psbb" name="kondisi" value="Belum Pernah">
+                    <label for="psbb">Belum Pernah</label>
                   </div>
 
                   <div class="form-group">
                     <label for="exampleInputNama1">20. Bantuan apa yang saat ini dibutuhkan oleh instansi Anda dalam menghadapi Covid-19 ?</label>
-                    <input type="text" name="nohp" class="form-control" id="exampleInputNoHP" placeholder="">
+                    <input type="text" name="bantuan" class="form-control" id="exampleInputNoHP" placeholder="Sebutkan">
                   </div>
 
                 </div>
